ffcd5f57733646ff3ae5b82e81178343c14a81ee
leticiabernardo <leticia.bernardo@mercadolivre.com>
2020-01-18 09:39:32 -0300

    Add search_like function: words that look like the anagram

--- search.py
+++ search.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime 
 import utils.helpers as helpers
 
 
@@ -32,13 +32,30 @@ class Search:
         existing_anagrams = list()
 
         sorted_word = ''.join(sorted(helpers.clean_word(anagram)))
-        start_time = datetime.datetime.now().time().strftime('%H:%M:%S.%f')
+        start_time = datetime.now().time().strftime('%H:%M:%S.%f')
 
         if sorted_word in self.my_dictionary.keys():
             existing_anagrams = self.my_dictionary[sorted_word]
 
-        end_time = datetime.datetime.now().time().strftime('%H:%M:%S.%f')
-        total_time = (datetime.datetime.strptime(end_time, '%H:%M:%S.%f') -
-                      datetime.datetime.strptime(start_time, '%H:%M:%S.%f'))
+        end_time = datetime.now().time().strftime('%H:%M:%S.%f')
+        total_time = (datetime.strptime(end_time, '%H:%M:%S.%f') -
+                      datetime.strptime(start_time, '%H:%M:%S.%f'))
 
         return total_time, existing_anagrams
+
+
+    def search_like_anagrams(self, anagram):
+        existing_anagrams = list()
+
+        sorted_word = ''.join(sorted(helpers.clean_word(anagram)))
+        start_time = datetime.now().time().strftime('%H:%M:%S.%f')
+
+        for key_word in self.my_dictionary.keys():
+            if sorted_word in key_word:
+                existing_anagrams += self.my_dictionary[key_word]
+
+        end_time = datetime.now().time().strftime('%H:%M:%S.%f')
+        total_time = (datetime.strptime(end_time, '%H:%M:%S.%f') -
+                      datetime.strptime(start_time, '%H:%M:%S.%f'))
+
+        return total_time, existing_anagrams 
